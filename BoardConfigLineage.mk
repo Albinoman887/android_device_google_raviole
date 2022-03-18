df5733ff2e915efdf020c7a7c23933108131bb82
Matt Filetto <matt.filetto@gmail.com>
2022-03-17 22:38:45 -0700

    raviole: enable quick-tap feature
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I380013eb21cc94b034434d4fb209e8b3d9529672

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,4 +1,7 @@
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
+# Quick-tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # UDFPS
 TARGET_HAS_UDFPS := true
