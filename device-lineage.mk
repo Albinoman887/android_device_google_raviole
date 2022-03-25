cab5503dc179390b5860325e101e9ad64d91bc56
Matt Filetto <matt.filetto@gmail.com>
2022-03-25 03:01:30 -0700

    raviole: create device specific artifact path requirement allowlist
    * GSI Devices are more strict. Instead of making a bunch of PRs all over
     for just one device , Just keep a full list here.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: Ie3307ff8b8718ceb307cc48dc4d751ff59a23da2

--- device-lineage.mk
+++ device-lineage.mk
@@ -95,4 +95,24 @@ TARGET_SUPPORTS_QUICK_TAP := true
 # UDFPS
 TARGET_HAS_UDFPS := true
 
+# Artifact path requirement allowlist
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/etc/permissions/android.hardware.biometrics.face.xml \
+    system/etc/permissions/privapp-permissions-com.crdroid.faceunlock.xml \
+    system/etc/sysconfig/hiddenapi-whitelist-com.crdroid.faceunlock.xml \
+    system/lib64/faceunlock_vendor_dependencies.so \
+    system/lib64/libFaceDetectCA.so \
+    system/lib64/libMegviiUnlock-jni-1.2.so \
+    system/lib64/libMegviiUnlock.so \
+    system/lib64/libarcsoft-lib.so \
+    system/lib64/libarcsoft_faceid.so \
+    system/lib64/libarcsoftbase.so \
+    system/lib64/libmegface.so \
+    system/lib64/libmpbase.so \
+    system/priv-app/FaceUnlockService/FaceUnlockService.apk \
+    system/addon.d/70-velvet.sh \
+    system/app/FlipendoPrebuilt/FlipendoPrebuilt.apk \
+    system/app/GoogleExtShared/GoogleExtShared.apk \
+    system/etc/permissions/privapp-permissions-google.xml \
+    system/priv-app/GooglePackageInstaller/GooglePackageInstaller.apk
 
