fc22452e1f1ef98cdf97b4c4ece9767a28ed6243
Matt Filetto <matt.filetto@gmail.com>
2022-03-29 04:54:49 -0700

    Build with GMS
    Change-Id: I8d6a92129c16f6c292f43bdf5915d3a9e54e30ec
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I3b51ba255335ec3ebbc7df58f1c3dee0038f1565

--- lineage_raven.mk
+++ lineage_raven.mk
@@ -9,7 +9,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_raven.mk)
-$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+$(call inherit-product, vendor/gms/products/gms.mk)
+WITH_GMS := true
 
 include device/google/gs101/lineage_common.mk
 include device/google/raviole/device-lineage.mk
