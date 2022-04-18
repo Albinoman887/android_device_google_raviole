9994b66fd9d4d1e0d4a8357c409cdf1a6bda618a
Matt Filetto <matt.filetto@gmail.com>
2022-04-18 02:04:32 -0700

    WORKING_DIR: drop some overlays
    Change-Id: I5af998a6e476ba0b81282ef454ce3e4fca5988db

--- device-lineage.mk
+++ device-lineage.mk
@@ -20,10 +20,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
-# EUICC
-PRODUCT_PACKAGES += \
-    EuiccSupportPixelOverlay
-
 # Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
