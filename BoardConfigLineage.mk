69b296de8d0bd11c4de0d744601cf1497b266c50
Matt Filetto <matt.filetto@gmail.com>
2022-05-26 02:49:31 -0700

    SparkOS -> CrDroid
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: Ifa18ee50fe5ac5e815d81f400897508897683fab

---
+++ BoardConfigLineage.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2021 The SparkOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
--- BoardConfigSpark.mk
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2021 The SparkOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/spark/config/device_framework_matrix.xml
