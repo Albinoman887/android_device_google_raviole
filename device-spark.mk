10e8144c93a0b541eb6410d080715b1ce513ed05
Matt Filetto <matt.filetto@gmail.com>
2022-09-26 19:34:48 -0700

    raviole: enable Quick-tap support
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I64d1afd7138c7c24b5eceda8fec09760dcf787ce
    
    # Conflicts:
    #	device-lineage.mk

--- device-spark.mk
+++ device-spark.mk
@@ -4,3 +4,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-spark
 # Gapps
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 WITH_GAPPS := true
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
