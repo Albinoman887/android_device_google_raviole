ad9cf07339de09c29e85209f77bc2c038f30678d
Matt Filetto <matt.filetto@gmail.com>
2022-09-27 01:44:47 -0700

    raviole: Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-spark.mk
+++ device-spark.mk
@@ -1,2 +1,8 @@
 # artifacts
 $(call inherit-product, device/google/raviole/artifacts.mk)
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
