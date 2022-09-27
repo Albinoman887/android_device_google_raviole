af2460858641d1f2905d108eba14eeae2f7bff44
Matt Filetto <matt.filetto@gmail.com>
2022-09-26 19:46:13 -0700

    raviole: include generic device compatibility matrix from vendor/spark by default
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- raven/BoardConfig.mk
+++ raven/BoardConfig.mk
@@ -22,6 +22,7 @@ else
   TARGET_SCREEN_DENSITY := 560
   USES_DEVICE_GOOGLE_RAVIOLE := true
 
+  include device/google/raviole/BoardConfigSpark.mk
   include device/google/gs101/BoardConfig-common.mk
   include device/google/gs101/wifi/BoardConfig-wifi.mk
   -include vendor/google_devices/gs101/prebuilts/BoardConfigVendor.mk
